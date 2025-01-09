0e754daee97d6384262ce855604a6c72510e1edd
Usman091020 <usmanpes2018@gmail.com>
2025-01-10 01:21:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// You can add interactive features here, like a simple alert for the search form.
+document.querySelector('.search-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Searching for properties...');
+});
